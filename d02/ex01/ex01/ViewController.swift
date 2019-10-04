96c5cd3aab70a3106718514bc4a874bd9487edaa
Ivan Kozlov <ikozlov@student.42.us.org>
2019-10-04 15:18:33 -0700

    d02 ex01

---
+++ d02/ex01/ex01/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ex01
+//
+//  Created by Ivan Kozlov on 10/4/19.
+//  Copyright © 2019 Ivan Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var timeLabel: UILabel!
+    
+    let startTime: TimeInterval = 25 * 60;
+    
+    var time: TimeInterval = 0;
+    var timer: Timer = Timer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resetTimer()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func updateTimeLabel() {
+        let formatter = DateFormatter()
+        let date = Date(timeIntervalSinceReferenceDate: -(16 * 3600) + time)
+        
+        formatter.dateFormat = "HH:mm:ss"
+        timeLabel.text = formatter.string(from: date)
+    }
+
+    @IBAction func startTimer() {
+        if timer.isValid {
+            return
+        }
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.time -= timer.timeInterval
+            self.updateTimeLabel()
+        })
+    }
+    
+    @IBAction func stopTimer() {
+        timer.invalidate()
+    }
+
+    @IBAction func resetTimer() {
+        stopTimer()
+        time = startTime;
+        updateTimeLabel()
+    }
+}
+
